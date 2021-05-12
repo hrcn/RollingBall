0d2651e595632deb47259186dd6ac99290338644
hrcn <hrcn1128@gmail.com>
2021-05-12 11:30:43 +0800

    player and camera movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody rd;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // Debug.Log("Game started!");
+        rd = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rd.AddForce(new Vector3(h, 0, v));
     }
 }
