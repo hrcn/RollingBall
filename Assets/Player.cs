2a8cde11d3363439dcbb52e11228c0303c07e988
hrcn <hrcn1128@gmail.com>
2021-05-13 12:56:01 +0800

    added UI

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody rd;
+    public int score = 0;
+    public Text scoreText;
+    public GameObject winText;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,21 @@ public class Player : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        rd.AddForce(new Vector3(h, 0, v));
+        rd.AddForce(new Vector3(h, 0, v) * 2);
+
+        if (score == 5)
+        {
+            winText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Goal")
+        {
+            Destroy(other.gameObject);
+            score++;
+            scoreText.text = "Score: " + score;
+        }
     }
 }
